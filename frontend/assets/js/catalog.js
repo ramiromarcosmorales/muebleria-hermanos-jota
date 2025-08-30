1eddb34a0f2aa304c39cce8b141c4b9019cddca5
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-08-30 20:54:12 -0300

    fix(catalog.js): formato de precios corregido

--- frontend/assets/js/catalog.js
+++ frontend/assets/js/catalog.js
@@ -1,47 +1,49 @@
+import { formatPrice } from "./format-price.js";
 import { products } from "./products.js";
 
 function getProductsAsync() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (products && products.length > 0) {
-                resolve(products);
-            } else {
-                reject("No hay productos disponibles.");
-            }
-        }, 1000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (products && products.length > 0) {
+        resolve(products);
+      } else {
+        reject("No hay productos disponibles.");
+      }
+    }, 1000);
+  });
 }
 
 function renderProducts(products, container) {
-    let productsHtml = '';
+  let productsHtml = "";
 
-    products.forEach(p => {
-        productsHtml += 
-        `
+  products.forEach((p) => {
+    productsHtml += `
             <article>
                 <h3>${p.nombre}</h3>
                 <img src="${p.srcImg}" alt="${p.nombre}" />
-                <p>Precio: $${p.precio}</p>
-                <a href="producto.html?id=${p.id}" class="product-button">Ver Producto</a>
+                <p>Precio: ${formatPrice(p.precio)}</p>
+                <a href="producto.html?id=${
+                  p.id
+                }" class="product-button">Ver Producto</a>
             </article>
         `;
-    });
-    container.innerHTML = productsHtml;
+  });
+  container.innerHTML = productsHtml;
 }
 
 async function initCatalog() {
-    const container = document.getElementById("container");
-    if (!container) return;
+  const container = document.getElementById("container");
+  if (!container) return;
 
-    container.innerHTML = `<p role="status" aria-live="polite">Cargando productos...</p>`
+  container.innerHTML = `<p role="status" aria-live="polite">Cargando productos...</p>`;
 
-    try {
-        const data = await getProductsAsync()
-        renderProducts(data, container);
-    } catch (e) {
-        container.innerHTML = `<p>No hay productos disponibles.</p>`
-        console.error("Error: ", e)        
-    }
+  try {
+    const data = await getProductsAsync();
+    renderProducts(data, container);
+  } catch (e) {
+    container.innerHTML = `<p>No hay productos disponibles.</p>`;
+    console.error("Error: ", e);
+  }
 }
 
 initCatalog().catch(console.error);
