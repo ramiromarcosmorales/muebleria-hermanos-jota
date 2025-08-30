1400ae76921a6ab54ac3f483fce51033d4fd4440
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-08-30 20:55:36 -0300

    fix(product-detail-routing.js): formato de precios corregido

--- frontend/assets/js/product-detail-routing.js
+++ frontend/assets/js/product-detail-routing.js
@@ -1,27 +1,28 @@
+import { formatPrice } from "./format-price.js";
 import { products } from "./products.js";
 
 const params = new URLSearchParams(window.location.search);
 
 const productId = parseInt(params.get("id"));
 
-const product = products.find(p => p.id === productId);
+const product = products.find((p) => p.id === productId);
 
 const detailContainer = document.getElementById("product-detail");
 
 if (product) {
-      detailContainer.innerHTML = `
+  detailContainer.innerHTML = `
         <div>
             <img src=${product.srcImg} alt=${product.descripcion}>
         </div>
         <div>
             <a href="productos.html">← Volver a productos</a>
             <h2>${product.nombre}</h2>
-            <p>Precio: $${product.precio}</p>
+            <p>Precio: ${formatPrice(product.precio)}</p>
             <p>En stock - Envío en 3-5 días hábiles</p>
             <p>${product.descripcion}</p>
             <button>Agregar al carrito</button>
         </div>
       `;
 } else {
-      detailContainer.innerHTML = `<p>Producto no encontrado</p>`;
+  detailContainer.innerHTML = `<p>Producto no encontrado</p>`;
 }
