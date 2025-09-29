a5d5260e91557ae39c01c9c497fffaacddce6bff
Sebastian Pallero <sebastianpallerodev@gmail.com>
2025-09-29 20:21:39 -0300

    fix(index): Se arregla el nombre del index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { getJSON } from "./utils/api";
+
+getJSON("/api/productos").then(console.log);
 
 function App() {
   const [cart] = useState([]);
