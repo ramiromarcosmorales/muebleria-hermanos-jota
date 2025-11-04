ca09861fc5755510e07609576eb1fbecc84c8ece
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-11-04 11:24:29 -0300

    feat(authenticationService.js): servicio de registro

--- backend/service/authenticationService.js
+++ backend/service/authenticationService.js
@@ -1,1 +1,26 @@
-export default async function registerUser(userData) {}
+import { User } from "../models/Users";
+import { validateUser } from "../utils/validateUser";
+
+export default async function registerUser(userData) {
+  const errors = validateUser(userData);
+  if (errors.length > 0) {
+    const error = new Error("Error de validación");
+    error.details = errors;
+    throw error;
+  }
+
+  // Se busca si hay un usuario registrado con el mismo email
+  const userAlreadyExists = await User.findOne({
+    correoElectronico: userData.email,
+  });
+  if (userAlreadyExists) {
+    const error = new Error(
+      "Existe un usuario registrado con ese correo electrónico."
+    );
+    throw error;
+  }
+
+  const newUser = new User(userData);
+
+  return await newUser.save();
+}
