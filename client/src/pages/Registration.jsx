4a60fdb48eb3d7b1412fa6b14001547ddec74522
Fernando David Medvedovsky <fmedvedovsky@gmail.com>
2025-11-04 10:57:00 -0300

    feat(Registration.jsx): validación del formulario y mensajes de estado de envío

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -16,8 +16,69 @@ export default function Registration() {
 
   const [errors, setErrors] = useState([]);
 
+  // Constantes para la validación del form
+  const NAME_MIN_LENGTH = 3;
+  const NAME_MAX_LENGTH = 100;
+  const PASSWORD_MIN_LENGTH = 6;
+  const EMAIL_REGEX =
+    // eslint-disable-next-line no-control-regex
+    /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+
+  function validateForm() {
+    const errors = [];
+
+    if (
+      !form.username ||
+      form.username.length < NAME_MIN_LENGTH ||
+      form.username.length > NAME_MAX_LENGTH
+    ) {
+      errors.push(
+        `El nombre de usuario debe tener entre ${NAME_MIN_LENGTH} y ${NAME_MAX_LENGTH} caracteres.`
+      );
+    }
+
+    if (!form.email || !EMAIL_REGEX.test(form.email)) {
+      errors.push(
+        "El correo electrónico debe cumplir con el formato adecuado."
+      );
+    }
+
+    if (!form.password || form.password.length < PASSWORD_MIN_LENGTH) {
+      errors.push(
+        `La contraseña debe tener al menos ${PASSWORD_MIN_LENGTH} caracteres.`
+      );
+    }
+
+    return errors;
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    const errors = validateForm();
+    if (errors.length > 0) {
+      setErrors(errors);
+      setStatus(STATUS_CLASSNAMES_ENUM.ERROR);
+      return;
+    }
+    setStatus(STATUS_CLASSNAMES_ENUM.SUCCESS);
+    console.log("Formulario enviado:", form);
+  }
+
+  function displayErrors() {
+    return (
+      <>
+        <p>No se pudo enviar el formulario por los siguientes errores:</p>
+        <ul>
+          {errors.map((e, i) => (
+            <li key={i}>{e}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  function displaySuccess() {
+    return <p>Formulario enviado exitosamente.</p>;
   }
 
   function handleChange(event) {
@@ -69,7 +130,10 @@ export default function Registration() {
           ></input>
         </div>
 
-        <div className={status}></div>
+        <div className={status}>
+          {status === STATUS_CLASSNAMES_ENUM.ERROR && displayErrors()}
+          {status === STATUS_CLASSNAMES_ENUM.SUCCESS && displaySuccess()}
+        </div>
 
         <button type="submit">Finalizar registro</button>
       </form>
